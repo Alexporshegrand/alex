0e6b0141ccf0dc67c702b1f1abe4d622b54ed759
Alex <84225@mail.ru>
2023-05-12 21:21:45 +0400

    пробывал в main py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Привет мир")
+print(2*3)
+print(600/5)
+print(145*35/437)
+print("hello vorld")
+print(2//3)
+print(2%3)
+type_int=1 #числа с точкой
+type_str=#"строки пишуться в кавычках"
+type_bool_one= True #булевы значение истина(правда)
+type_bool_two= True# булевы значение ложь
+type_none= None
+
