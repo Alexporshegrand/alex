122fa33fd6497983dd82d4ba1fe3e016f0efef0b
Alex <84225@mail.ru>
2023-05-16 21:24:55 +0400

    переменные

--- main.py
+++ main.py
@@ -5,9 +5,36 @@ print(145*35/437)
 print("hello vorld")
 print(2//3)
 print(2%3)
-type_int=1 #числа с точкой
-type_str=#"строки пишуться в кавычках"
-type_bool_one= True #булевы значение истина(правда)
-type_bool_two= True# булевы значение ложь
-type_none= None
+# type_int=1 #числа с точкой int
+# type_int=1.23 #числа с точкой float
+# type_str=#"строки пишуться в кавычках"
+# type_bool_one= True #булевы значение истина(правда)
+# type_bool_two= True# булевы значение ложь
+# type_none= None""
+#  a=2
+# print(a, type(a))#int
+# a=str(a, type(a))#str
+# b=2
+# z =input("ВВедите текст:")# input по умолчанию строка 
+# print(z, type(z))
+# print(z)
+# z =input("ВВедите мы умножим его на 2")# input по умолчанию строка
+# z =int(z)# из строки сделали число
+# print(z*2)
+
+print("Заполните информацию")
+myname=input ("введите имя:")
+print(myname)
+myage=input ("введите ваш возраст")
+myage=str(myage)
+print(myage)
+myhobby=input ("ваше любимое хобби?")
+print(myhobby)
+mykinder=input (" сколько у вас детей?")
+mykinder=str
+print(mykinder)
+
+
+
+
 
