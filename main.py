a5a55c42ed7b796a8a534c59591fc63ec22173f7
Alex <84225@mail.ru>
2023-05-19 20:29:03 +0400

    add

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-print("Привет мир")
-print(2*3)
-print(600/5)
-print(145*35/437)
-print("hello vorld")
-print(2//3)
-print(2%3)
+# print("Привет мир")
+# print(2*3)
+# print(600/5)
+# print(145*35/437)
+# print("hello vorld")
+# print(2//3)
+# print(2%3)
 # type_int=1 #числа с точкой int
 # type_int=1.23 #числа с точкой float
 # type_str=#"строки пишуться в кавычках"
@@ -22,19 +22,61 @@ print(2%3)
 # z =int(z)# из строки сделали число
 # print(z*2)
 
-print("Заполните информацию")
-myname=input ("введите имя:")
-print(myname)
-myage=input ("введите ваш возраст")
-myage=str(myage)
-print(myage)
-myhobby=input ("ваше любимое хобби?")
-print(myhobby)
-mykinder=input (" сколько у вас детей?")
-mykinder=str
-print(mykinder)
+# print("Заполните информацию")
+# myname=input ("введите имя:")
+# print(myname)
+# myage=input ("введите ваш возраст")
+# myage=str(myage)
+# print(myage)
+# myhobby=input ("ваше любимое хобби?")
+# print(myhobby)
+# mykinder=input (" сколько у вас детей?")
+# mykinder=str
+# print(mykinder)
+
+
+# a=5
+# b=10
+# if a<b:
+#     print("if выполнится")
+# if a>b:
+#     print("if 2 выполнится")
+# else:
+#     print("else выполнится")
 
 
 
+#     if a>b or b == 10
+#     print("if выполнится")
 
 
+
+q1 = input("Зимой и летом одним цветом : ")
+score = 0
+if q1 == "ёлка" or q1 == "елка":
+    print("Ответ верный")
+    score = score + 1
+else:
+    print("Ответ не верный")
+
+w1 = input("кто самый главный на Новый год : ")
+if w1 == ("Дед мороз" or "Снегурочка"):
+    print("Ответ верный")
+    score = score + 1
+else:
+    print("Ответ не верный")
+
+e1 = input("сколько сторон у треугольника : ")
+if e1 == "3" :
+    print("Ответ верный")
+    score = score + 1
+else:
+    print("Ответ не верный") 
+
+if  score < 3:
+    print("хотите ответить на дополнительный вопрос")
+    h = input("введите да или нет : ")
+    if h == "да":
+       print( "у меня нет больше вопросов")  
+else:
+    print( "вы молодец")
